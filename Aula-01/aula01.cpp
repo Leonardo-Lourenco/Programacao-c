4bd9466b1f3bc86dd9d6874025b5e7b7db7cd830
Leonardo Lourenco <lnddru@gmail.com>
2020-07-03 10:39:41 -0400

    Add files via upload

---
+++ Aula-01/aula01.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+/*	
+	int numero = 20;
+	float numeroDecimal = 20.45;
+	double numeroGrande = 20.1245;
+	char letra = 'c';
+	
+	cout << " Inteiro  " << numero << endl;
+	cout << " Decimal " << numeroDecimal << "\n";
+	cout << " Double  " << numeroGrande <<"\n";
+	cout << " Char  " << letra;
+			
+			
+			*/
+			
+			
+	float number;
+	
+	cout<<"Informe um número:  ";
+	cin>>number;
+	
+	cout<<" O número informado é: " <<number;		
+	
+	
+	return 0;
+	
+	}
