4ea860151150ad910752303d0615170adbe371c4
Leonardo Lourenco <lnddru@gmail.com>
2020-07-04 10:44:01 -0400

    Add files via upload

---
+++ Aula-03/aula03.cpp
@@ -0,0 +1,44 @@
+
+// IF / ELSE
+
+/*    if(condição) {
+ *   
+ * INSTRUÇÃO 1
+ * 
+ * 
+ * } else{
+ * 
+ *    INSTRUÇÃO 2
+ * 
+ * 
+ * } */
+
+ 
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	int valor, numero = 10;
+	
+	
+	cout<<"Digite um número:  ";
+	cin>>valor;
+	
+	if(valor == numero)
+	{
+		cout<<"O número é 10";
+	} else{
+		cout<<"O número é diferente de 10";
+	}
+	
+	
+	
+	return 0;
+	
+	}
+
+
